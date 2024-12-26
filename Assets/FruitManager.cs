1825714da76abbefabc7850c8f15c489ed107ea8
Shreya Thothathri <shreyathoth@vt.edu>
2024-12-25 22:25:44 -0500

    Added Fruit Count to Screen

--- Assets/FruitManager.cs
+++ Assets/FruitManager.cs
@@ -1,9 +1,13 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FruitManager : MonoBehaviour
 {
 
     public int fruitCount;
+    public TMP_Text fruitText;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +17,6 @@ public class FruitManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        fruitText.text = "Fruit Count: " + fruitCount.ToString();
     }
 }
